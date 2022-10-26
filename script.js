6910d8a408264c13cd825f4f17827184b88bd94c
alaeloula <105067668+alaeloula@users.noreply.github.com>
2022-10-26 21:29:11 +0100

    livrable 2

---
+++ script.js
@@ -0,0 +1,6 @@
+var  openBtn =document.getElementById("openBtn");
+var ktba2 =document.querySelector(".ktba2");
+openBtn.addEventListener("click",function(){
+   ktba2.classList.toggle('active');
+})
+
