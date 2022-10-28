680fb03ace62d45b84c621285f1fb188bf0f683f
alaeloula <105067668+alaeloula@users.noreply.github.com>
2022-10-28 17:31:16 +0100

    js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ openBtn.addEventListener("click",function(){
    ktba2.classList.toggle('active');
 })
 
+
+
+var  btnplat =document.getElementById("btnplat");
+var Plat =document.querySelector(".Plat");
+var produit =document.querySelector(".produit");
+Plat.classList.add('plat');
+btnplat.addEventListener("click",function(){
+   produit.classList.toggle('active');
+    Plat.classList.toggle('Plat');
+   // alert("clicked");
+})
+
+
---
+++ test.js
@@ -0,0 +1,52 @@
+let data=[
+    {
+        "id":"1",
+        "nom" : "nom1",
+        "prix" :"50"
+    },
+    {
+        "id" : "2",
+        "nom" : "nom1",
+        "prix" :"30",
+        
+    }
+];
+//  document.getElementById("add").innerHTML = "Hello World!";
+var heho=document.getElementById("heho")
+
+heho.addEventListener("click",function(){
+    document.getElementById("add").innerHTML = '<div class="pd"><img src="public/playground_assets/imagei582-gws-300h.png" alt="pd1" srcset=""><div class="txt" ><label for="">nom</label> <input type="submit" value="add" name="heho"></div></div>';
+ })
+// kandiruuuuuuuuuuuuuuuuu
+
+// var  heho =document.querySelectorAll(".heho");
+// heho.forEach((btn)=> {
+//     btn.addEventListener('click',function(){
+//         // for (let index = 0; index < data.length; index++) {
+//         //     const element = array[index];
+//         //     if(){
+                
+//         //     }
+//         // }
+//         alert(this.value);
+//     });
+// });
+
+
+// var  heho =document.querySelectorAll(".heho");
+//  heho.addEventListener("click",function(){
+//     document.getElementById("add").innerHTML = "data[1]";
+//   })
+
+
+
+
+
+
+
+
+
+
+
+
+
